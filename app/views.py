d605eef8cd38845ebd153254f79cd487127b13c8
Nazaraly Nadirbek <naznadmn@gmail.com>
2016-12-11 00:14:21 +0600

    Template changes / URL routing changes

--- app/views.py
+++ app/views.py
@@ -3,9 +3,22 @@
 from app import app
 
 # Flask modules
+from flask import redirect
 from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
+    return redirect('/articles/')
+
+@app.route('/about')
+def about():
+    return redirect('/articles/')
+
+@app.route('/articles/')
+def articles():
+    return render_template('default/content/articles.html')
+
+@app.route('/articles/<article>')
+def article(article):
     return render_template('default/content/articles.html')
